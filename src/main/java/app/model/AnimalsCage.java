994c0d00b6e548e814c218abfb3417321e0bae80
viacheslav belenchenkov <belenchenkov@gmail.com>
2025-12-17 20:58:03 +0300

    Initial commit: Spring Beans solution

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,13 +1,18 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+
 
 @Component
 public class AnimalsCage {
 
+    @Qualifier("dog")
     @Autowired
     private Animal animal;
+    @Autowired
+    private Timer timer;
 
     public void whatAnimalSay() {
         System.out.println("Say:");
@@ -16,4 +21,6 @@ public class AnimalsCage {
         System.out.println(new Timer().getTime());
         System.out.println("________________________");
     }
+
+    public Timer getTimer() { return timer; }
 }
